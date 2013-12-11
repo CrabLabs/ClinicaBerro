b2b76c92b57ea3d4f202e20ce10693eaef370351
Rodrigo Espinosa <espinosacurbelo@gmail.com>
2013-12-11 00:58:06 -0200

    Video

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+(function ($) {
+	'use strict';
+
+	$('video').on('click', function () {
+		if ($(this)[0].paused) {
+			$(this)[0].play();
+		} else {
+			$(this)[0].pause();
+		}
+	});
+
+}(jQuery));
