b1ca3e5f1a209c8ffb516e962f0b07b843fe0b1b
Rodrigo Espinosa <espinosacurbelo@gmail.com>
2013-12-11 03:47:12 -0200

    Video button

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,8 +4,10 @@
 	$('video').on('click', function () {
 		if ($(this)[0].paused) {
 			$(this)[0].play();
+			$('#play_button').fadeOut();
 		} else {
 			$(this)[0].pause();
+			$('#play_button').fadeIn();
 		}
 	});
 
